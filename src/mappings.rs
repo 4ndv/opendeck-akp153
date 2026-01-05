338122424ef39830e0fdb742b76381e56b91af56
Andrey Viktorov <loonks@disroot.org>
2026-01-05 23:38:58 +0700

    feat(device): add support for Womier D15 (#32)

--- src/mappings.rs
+++ src/mappings.rs
@@ -26,6 +26,7 @@ pub enum Kind {
     RMV01,
     SFSTC,
     TMICESC,
+    D15,
 }
 
 pub const AJAZZ_VID: u16 = 0x0300;
@@ -36,6 +37,7 @@ pub const MADDOG_VID: u16 = 0x0c00;
 pub const RISEMODE_VID: u16 = 0x0a00;
 pub const SF_STC_VID: u16 = 0x1500;
 pub const TMICE_VID: u16 = 0x0500;
+pub const WOMIER_VID: u16 = 0x0600;
 
 pub const HSV293S_PID: u16 = 0x6670;
 pub const HSV293SV3_PID: u16 = 0x1014;
@@ -54,10 +56,13 @@ pub const RMV01_PID: u16 = 0x1001;
 pub const SF_STC_PID: u16 = 0x3003;
 pub const TMICESC_PID: u16 = 0x1001;
 
+pub const D15_PID: u16 = 0x1000;
+
 // Map all queries to usage page 65440 and usage id 1 for now
 pub const HSV293S_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MIRABOX_VID, HSV293S_PID);
 pub const HSV293SV3_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MIRABOX_2_VID, HSV293SV3_PID);
-pub const HSV293SV3_1005_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MIRABOX_2_VID, HSV293SV3_1005_PID);
+pub const HSV293SV3_1005_QUERY: DeviceQuery =
+    DeviceQuery::new(65440, 1, MIRABOX_2_VID, HSV293SV3_1005_PID);
 pub const AKP153_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MIRABOX_VID, AKP153_PID);
 pub const AKP153E_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, AJAZZ_VID, AKP153E_PID);
 pub const AKP153E_REV2_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, AJAZZ_VID, AKP153E_REV2_PID);
@@ -67,8 +72,9 @@ pub const GK150K_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MADDOG_VID, GK1
 pub const RMV01_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, RISEMODE_VID, RMV01_PID);
 pub const SF_STC_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, SF_STC_VID, SF_STC_PID);
 pub const TMICESC_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, TMICE_VID, TMICESC_PID);
+pub const D15_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, WOMIER_VID, D15_PID);
 
-pub const QUERIES: [DeviceQuery; 12] = [
+pub const QUERIES: [DeviceQuery; 13] = [
     HSV293S_QUERY,
     HSV293SV3_QUERY,
     HSV293SV3_1005_QUERY,
@@ -81,6 +87,7 @@ pub const QUERIES: [DeviceQuery; 12] = [
     RMV01_QUERY,
     SF_STC_QUERY,
     TMICESC_QUERY,
+    D15_QUERY,
 ];
 
 /// Returns correct image format for device kind and key
@@ -155,6 +162,11 @@ impl Kind {
                 _ => None,
             },
 
+            WOMIER_VID => match pid {
+                D15_PID => Some(Kind::D15),
+                _ => None,
+            },
+
             _ => None,
         }
     }
@@ -186,6 +198,7 @@ impl Kind {
             Self::RMV01 => "Risemode Vision 01",
             Self::SFSTC => "Soomfon Stream Controller",
             Self::TMICESC => "TMICE Stream Controller",
+            Self::D15 => "Womier D15",
         }
         .to_string()
     }
@@ -202,6 +215,7 @@ impl Kind {
             Self::GK150K => "GK150K",
             Self::RMV01 => "RMV01",
             Self::TMICESC => "TMICESC",
+            Self::D15 => "D15",
             // This method would not be called for "v2"/"v3" devices, so mark them as unreachable
             Self::HSV293SV3 => unreachable!(),
             Self::HSV293SV3_1005 => unreachable!(),
